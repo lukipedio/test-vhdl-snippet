fec0229451872e0a38fb9ae95108cf61eec19aa9
Luca Colombini <l.colombini@caen.it>
2021-03-27 23:20:22 +0100

    Add code snippet from stackoverflow
    https://stackoverflow.com/questions/66832409/no-feasible-entry-to-subprogram-shift-left-type-error-near

---
+++ project.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+
+entity project is
+    port (
+        i_clk     : in std_logic;
+        i_rst     : in std_logic;
+        i_start   : in std_logic;
+        i_data    : in std_logic_vector(7 downto 0);
+    );
+end project;
+
+architecture rtl of project is
+    signal shifted_pixel, temp : std_logic_vector(15 downto 0);
+    begin
+        process(i_data)
+        begin
+            temp <= "00000000" & i_data;
+            shifted_pixel <= shift_left(temp, 5);
+        end process;
+end rtl;
